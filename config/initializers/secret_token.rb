1011bdb9f622e0afbd2fbb666e6fc760a6c77785
erhanatl <erhanatl@gmail.com>
2013-01-20 20:24:23 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iddaa::Application.config.secret_token = 'ce0f350016c952194d95f5f75b573767269b3097e7b1fc9ac9d4636f94b0edfca37ec81d0129ba75e498462b82dbafc3170dfaabcf6f44fca5e12edea8465f75'
