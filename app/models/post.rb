88f29805075d1835b70a305fa5270627559f68f9
erhanatl <erhanatl@gmail.com>
2013-01-20 21:14:42 +0200

    Second Commit

---
+++ app/models/post.rb
@@ -0,0 +1,9 @@
+class Post < ActiveRecord::Base
+  belongs_to :user
+  validates :kod, presence: true
+  validates :ev_sahibi, presence: true
+  validates :misafir, presence: true
+  validates :oran, presence: true
+  validates :tahmin, presence: true
+  attr_accessible :ev_sahibi, :kod, :misafir, :oran, :tahmin, :user_id
+end
